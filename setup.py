533b8e771424a4ffe20e2e8c7b2c415762523a18
Jayesh Nandi <iamsirjayesh@users.noreply.github.com>
2016-11-04 16:45:41 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
 # nepal_stock
+from distutils.core import setup
+setup(
+  name = 'nepal_stock',
+  packages = ['nepal_stock'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A lib for fetching current  Nepal Stock Exchange Value for different Trading Companies',
+  author = 'Jayesh Nandi',
+  author_email = 'jayesh_nandi2002@yahoo.com',
+  url = 'https://github.com/iamsirjayesh/nepal_stock', # use the URL to the github repo
+  download_url = 'https://github.com/iamsirjayesh/nepal_stock', # I'll explain this in a second
+  keywords = ['nepse', 'nepal stock', 'stock','bank','trade'], # arbitrary keywords
+  classifiers = [],
+)from distutils.core import setup
+setup(
+  name = 'nepal_stock',
+  packages = ['nepal_stock'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A lib for fetching current  Nepal Stock Exchange Value for different Trading Companies',
+  author = 'Jayesh Nandi',
+  author_email = 'jayesh_nandi2002@yahoo.com',
+  url = 'https://github.com/iamsirjayesh/nepal_stock', # use the URL to the github repo
+  download_url = 'https://github.com/iamsirjayesh/nepal_stock', # I'll explain this in a second
+  keywords = ['nepse', 'nepal stock', 'stock','bank','trade'], # arbitrary keywords
+  classifiers = [],
+)
